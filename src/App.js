50b338a122f3ff209c174777c5b8f71295be13f3
Nafis Abedin <nafisabedin@MacBook-Pro.local>
2020-05-17 17:36:21 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+
+import Layout from './hoc/Layout/Layout';
+
+import Welcome from './containers/Welcome/Welcome';
+import Auth from './containers/Auth/Auth';
+import Users from './containers/Users/Users';
+import Albums from './containers/Albums/Albums';
+import Photos from './containers/Photos/Photos';
+import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
+
+
+class App extends React.Component {
+
+  componentDidMount () {
+    this.props.onTryAutoSignup();
+  }
+
+
+
+  render(){
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={Welcome} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  
+    if (this.props.isAuth ) {
+      routes = (
+        <Switch>
+          <Route path="/users" component={Users} />
+          <Route path="/albums" component={Albums} />
+          <Route path="/photos" component={Photos} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={Welcome} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
+      return (
+        <div className="App">
+          <Layout>
+             {routes}
+          </Layout>
+        </div>
+      )
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
+  };
+};
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch( actions.authCheckState() )
+  };
+};
+
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
+
+/* 
+MD Nafis Abedin
+Mavennet Challenge
+*/
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+
+import App from './App';
+import fetcheducer from './store/reducers/fetch';
+import authReducer from './store/reducers/auth';
+
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+
+const rootReducer = combineReducers({
+    search: fetcheducer,
+    auth: authReducer
+});
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk)
+));
+
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App className="App" />
+        </BrowserRouter>
+    </Provider>
+        
+);
+
+ReactDOM.render( app, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+/* 
+MD Nafis Abedin
+Mavennet Challenge
+*/
